3985e7fc3bb66480d469e7ec32208b6aa401bffc
Cyber-Freak999 <cyberfreak999@protonmail.com>
2024-01-20 17:56:19 +0100

    feat: Added Payment Documentation

--- SDK Submissions/chimoney_rust_sdk/src/payment/payment.rs
+++ SDK Submissions/chimoney_rust_sdk/src/payment/payment.rs
@@ -2,6 +2,49 @@ use crate::core::api_client::APIClient;
 use serde_json::json;
 
 /// Initiate a payment request
+///
+/// # Arguments
+///
+/// * `api_client` - An instance of the `APIClient` used to make API requests.
+/// * `value_in_usd` - The value of the payment in USD.
+/// * `payer_email` - The email of the payer.
+/// * `currency` - The currency of the payment.
+/// * `amount` - The amount of the payment.
+/// * `redirect_url` - The redirect URL of the payment.
+/// * `sub_account` - The sub-account of the payment.
+/// * `meta_key` - The meta key of the payment.
+/// * `meta_value` - The meta value of the payment.
+///
+/// # Returns
+///
+/// A `Result` containing a JSON string with the payment details if successful,
+/// or an error if the request fails.
+///
+/// # Examples
+///
+/// ```rust
+/// use chimoney::core::api_client::APIClient;
+/// use chimoney::payment::payment::initiate_payment;
+///
+/// # #[tokio::main]
+/// # async fn main() {
+/// let api_client = APIClient::new("https://api.example.com");
+/// let value_in_usd = "100";
+/// let payer_email = "example@example";
+/// let currency = "USD";
+/// let amount = "100";
+/// let redirect_url = "https://example.com";
+/// let sub_account = "1234567";
+/// let meta_key = "key";
+/// let meta_value = "value";
+///
+/// match initiate_payment(&api_client, value_in_usd, payer_email, currency, amount, redirect_url, sub_account, meta_key, meta_value).await {
+///   Ok(result) => println!("Payment: {}", result),
+///  Err(err) => eprintln!("Error: {}", err),
+/// }
+/// # }
+/// ```
+///
 pub async fn initiate_payment(
     api_client: &APIClient,
     value_in_usd: &str,
@@ -32,6 +75,37 @@ pub async fn initiate_payment(
 }
 
 /// Verify a payment
+///
+/// # Arguments
+///
+/// * `api_client` - An instance of the `APIClient` used to make API requests.
+/// * `sub_account` - The sub-account of the payment.
+/// * `id` - The ID of the payment.
+///
+/// # Returns
+///
+/// A `Result` containing a JSON string with the payment details if successful,
+/// or an error if the request fails.
+///
+/// # Examples
+///
+/// ```rust
+/// use chimoney::core::api_client::APIClient;
+/// use chimoney::payment::payment::verify_payment;
+///
+/// # #[tokio::main]
+/// # async fn main() {
+/// let api_client = APIClient::new("https://api.example.com");
+/// let sub_account = "1234567";
+/// let id = "1234567";
+///
+/// match verify_payment(&api_client, sub_account, id).await {
+///  Ok(result) => println!("Payment: {}", result),
+/// Err(err) => eprintln!("Error: {}", err),
+/// }
+/// # }
+/// ```
+///
 pub async fn verify_payment(
     api_client: &APIClient,
     sub_account: &str,
@@ -49,6 +123,39 @@ pub async fn verify_payment(
 }
 
 /// Simulate a card
+///
+/// # Arguments
+///
+/// * `api_client` - An instance of the `APIClient` used to make API requests.
+/// * `issue_id` - The issue ID of the card.
+/// * `status` - The status of the card.
+/// * `sub_account` - The sub-account of the card.
+///
+/// # Returns
+///
+/// A `Result` containing a JSON string with the card details if successful,
+/// or an error if the request fails.
+///
+/// # Examples
+///
+/// ```rust
+/// use chimoney::core::api_client::APIClient;
+/// use chimoney::payment::payment::simulate_card;
+///
+/// # #[tokio::main]
+/// # async fn main() {
+/// let api_client = APIClient::new("https://api.example.com");
+/// let issue_id = "1234567";
+/// let status = "active";
+/// let sub_account = "1234567";
+///
+/// match simulate_card(&api_client, issue_id, status, sub_account).await {
+/// Ok(result) => println!("Card: {}", result),
+/// Err(err) => eprintln!("Error: {}", err),
+/// }
+/// # }
+/// ```
+///
 pub async fn simulate_card(
     api_client: &APIClient,
     issue_id: &str,
